95b6a155cc34d62989eb9c4af28e52827552ed2a
a-ankitpatel <ankit22687@gmail.com>
2021-09-21 22:30:03 +0930

    add console log

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import useHttp from './hooks/http-hook';
 
 function App() {
 
-  console.log('checking console log in test branch');
+  console.log('in test branch');
 
   const [tasks, setTasks] = useState([]);
 
